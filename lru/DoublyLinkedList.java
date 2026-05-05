1294c4957a9b4a40cb42175bfbaf365669f99cdd
MohamedAdel <mohamedadel999922@gmail.com>
2026-05-05 22:16:32 +0300

    moveNodeToFront added successfully

--- lru/DoublyLinkedList.java
+++ lru/DoublyLinkedList.java
@@ -1,10 +1,43 @@
 package lru;
-public class DoublyLinkedList{
+
+public class DoublyLinkedList {
     Node head;
     Node tail;
 
-    public DoublyLinkedList(){
+    public DoublyLinkedList() {
         head = null;
         tail = null;
+    }
+
+    public void moveNodeToFront(Node node) {
+
+        if (node == null || node == head) {
+            return;
+        }
+
+        if (node.prev != null) {
+            node.prev.next = node.next;
+        }
+
+        if (node.next != null) {
+            node.next.prev = node.prev;
+        }
+
+        if (node == tail) {
+            tail = node.prev;
+        }
+
+        node.next = head;
+        node.prev = null;
+
+        if (head != null) {
+            head.prev = node;
+        }
+
+        head = node;
+
+        if (tail == null) {
+            tail = head;
+        }
     }
 }
