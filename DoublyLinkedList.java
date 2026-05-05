d7174c5e995fa78d3940ec5e1433d56405e33c2f
ammar mahmoud <Fcds.AmmarMahmoud260169@alex.edu.eg>
2026-05-05 22:30:35 +0300

    Insert at front (Task 3)

--- DoublyLinkedList.java
+++ DoublyLinkedList.java
@@ -6,4 +6,15 @@ public class DoublyLinkedList{
         head = null;
         tail = null;
     }
+    public void insertAtFront(int key ,int value){
+        Node start = new Node(key, value);
+        if (head == null){
+            head = start;
+            tail = start;
+        } else {
+            start.next = head;
+            head.prev = start;
+            head = start;
+        }
+    }
 }
