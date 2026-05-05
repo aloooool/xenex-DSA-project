b643c2d9b6ecd274ad01791817310c5f98fa6603
Ali Elhassan <alielhassan620@gmail.com>
2026-05-05 23:10:54 +0300

    task3
    done by ammar

--- lru/DoublyLinkedList.java
+++ lru/DoublyLinkedList.java
@@ -1,12 +1,25 @@
 package lru;
 
-public class DoublyLinkedList {
+package lru;
+
+public class DoublyLinkedList{
     Node head;
     Node tail;
-
-    public DoublyLinkedList() {
+public DoublyLinkedList(){
         head = null;
         tail = null;
+    }
+
+    public void insertAtFront(int key ,int value){
+        Node start = new Node(key, value);
+        if (head == null){
+            head = start;
+            tail = start;
+        } else {
+            start.next = head;
+            head.prev = start;
+            head = start;
+        }
     }
 
     public void moveNodeToFront(Node node) {
