28d51b2347d2bf14acef102b1bdcb53bcdd031a1
Omar Gaber <omar.gaber323@gmail.com>
2026-05-13 22:59:23 +0300

    Last Commit2

--- lru/HashTable.java
+++ lru/HashTable.java
@@ -2,16 +2,12 @@ package lru;
 public class HashTable {
     public DoublyLinkedList[] lista;
     public int capacity;
-    int total_table_size;
     public HashTable(int capacity) {
         this.capacity = capacity;
         this.lista = new DoublyLinkedList[capacity];
         for (int i = 0; i < capacity; i++) {
             lista[i] = new DoublyLinkedList(false); 
         }
-    }
-    public int get_tableSize(){
-        return total_table_size;
     }
     // comlexty o(1)
     public int hash(int key) {
@@ -25,17 +21,12 @@ public class HashTable {
     public void remove(int key){
         Node t = search(key);
         if (t != null) {
-            total_table_size--;
             lista[hash(key)].removeNode(t);   
         }
     }
     // comlexty o(1)
     public void insert(Node node){
-        if (total_table_size < capacity) {
-            total_table_size++;
-            lista[hash(node.key)].insertAtFront(node);   
-        }
-
+        lista[hash(node.key)].insertAtFront(node);   
     }
 
 }
